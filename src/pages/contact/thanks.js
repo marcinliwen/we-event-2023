5987c81cc59f674bb074b002c62501c2e0176bb6
m.liwen <m.liwen@perfectone.pl>
2023-03-12 22:31:57 +0100

    form-name

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -6,17 +6,21 @@ import { Link } from "gatsby";
 // eslint-disable-next-line
 export default (props) => {
   return (
-  <Layout location={props.location.pathname}>
-    
-    <section className="section">
-      <div className="container">
-        <div className="my-20">
-          <div className="text-center mb-4">
-           <StaticImage src="../../img/confirm.png"  placeholder="none" width="150px" height="150px"/>
-          </div>
-          <div >
-            <div >
-             {/*  <p class="thank-you-icon" style="text-align: center;">
+    <Layout location={props.location.pathname}>
+      <section className="section">
+        <div className="container">
+          <div className="my-20">
+            <div className="text-center mb-4">
+              <StaticImage
+                src="../../img/confirm.png"
+                placeholder="none"
+                width="150px"
+                height="150px"
+              />
+            </div>
+            <div>
+              <div>
+                {/*  <p class="thank-you-icon" style="text-align: center;">
                 <img
                   class="notActiveTyIcon"
                   style="display: none;"
@@ -26,28 +30,32 @@ export default (props) => {
                   height="156"
                 />
               </p> */}
-              <div style={{textAlign: 'center'}}>
-                <h1
-                 className="font-bold text-5xl mb-8"
-                >
-                  Thank you!
-                </h1>
-                <p
-                  style={{textAlign: 'center'}}
-                >
-                  We have received your message!
-                </p>
-                <Link
-                  style={{display: 'block', width: 'max-content', margin: '32px auto', fontSize: '24px', color: '#fff', background: '#9dc017', padding: '16px 24px', textDecoration: 'none'}}
-                  href="/"
-                >
-                  Back to we2022.eu
+                <div style={{ textAlign: "center" }}>
+                  <h1 className="font-bold text-5xl mb-8">Thank you!</h1>
+                  <p style={{ textAlign: "center" }}>
+                    We have received your message!
+                  </p>
+                  <Link
+                    style={{
+                      display: "block",
+                      width: "max-content",
+                      margin: "32px auto",
+                      fontSize: "24px",
+                      color: "#fff",
+                      background: "#9dc017",
+                      padding: "16px 24px",
+                      textDecoration: "none",
+                    }}
+                    href="/"
+                  >
+                    Back to we-event.eu
                   </Link>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  </Layout>)
+      </section>
+    </Layout>
+  );
 };

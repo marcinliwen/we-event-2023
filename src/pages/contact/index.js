6d254edcea26b17dd03414a501d90bcdf1297af3
m.liwen <m.liwen@perfectone.pl>
2023-03-12 23:15:31 +0100

    test 1

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -25,7 +25,7 @@ export default class Index extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault();
     const form = e.target;
-    fetch("/", {
+    fetch("/.netlify/functions/submission-created", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({

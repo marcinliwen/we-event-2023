1546db195f268437cda820d83b73afd36b7409d3
m.liwen <m.liwen@perfectone.pl>
2023-03-17 00:10:21 +0100

    template id

--- netlify/functions/submission-created/submission-created.js
+++ netlify/functions/submission-created/submission-created.js
@@ -3,20 +3,35 @@ const sgMail = require("@sendgrid/mail");
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const handler = async (event) => {
+const handler = async (event, context) => {
   console.log("event: ", event);
+  console.log("context", context);
   try {
     const { data } = JSON.parse(event.body).payload;
 
-    const msg = {
-      to: data.email, // Change to your recipient
-      from: process.env.SENDER_EMAIL, // Change to your verified sender
-      subject: "Sending with SendGrid is Fun",
-      text: "and easy to do anywhere, even with Node.js",
-      html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-      name: data.name,
-      template_id: "d-f8c24a04d6314ea9a6e035933de4d34f",
-    };
+    console.log(data["form-name"]);
+    let msg = {};
+    switch (data["form-name"]) {
+      case "contact":
+        msg = {
+          to: data.email, // Change to your recipient
+          from: process.env.SENDER_EMAIL, // Change to your verified sender
+          subject: "Thank you for contakt us!",
+          text: "Thank you for contakt us!",
+          name: data.name,
+          template_id: "d-f8c24a04d6314ea9a6e035933de4d34f ",
+        };
+      case "stand_application_form":
+        msg = {
+          to: data.email, // Change to your recipient
+          from: process.env.SENDER_EMAIL, // Change to your verified sender
+          subject: "Thank you for submitting your application",
+          text: "The organizer will be in touch with you shortly",
+          name: data.name,
+          template_id: "d-2497f4e79b5941ca9bd6a626cf94441d",
+        };
+    }
+
     console.log(`Received a submission: ${data.email}`);
     await sgMail
       .send(msg)

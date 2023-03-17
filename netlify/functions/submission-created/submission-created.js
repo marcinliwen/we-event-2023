35bb54ca43920815908b5c70a49fd9cdee53dc2c
m.liwen <m.liwen@perfectone.pl>
2023-03-17 12:39:39 +0100

    try 3

--- netlify/functions/submission-created/submission-created.js
+++ netlify/functions/submission-created/submission-created.js
@@ -6,46 +6,41 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const handler = async (event, context) => {
   console.log("event: ", event);
   console.log("context", context);
-  try {
-    const { data } = JSON.parse(event.body).payload;
+  const { data } = JSON.parse(event.body).payload;
 
-    console.log(data["form-name"]);
-    let msg = {};
-    switch (data["form-name"]) {
-      case "contact":
-        msg = {
-          to: data.email,
-          from: process.env.SENDER_EMAIL, // Change to your verified sender
-          subject: "Thank you for contakt us!",
-          text: "Thank you for contakt us!",
-          template_id: "d-f8c24a04d6314ea9a6e035933de4d34f",
-        };
-        break;
-      case "stand_application_form":
-        msg = {
-          to: data.email,
-          from: process.env.SENDER_EMAIL, // Change to your verified sender
-          subject: "Thank you for submitting your application",
-          text: "The organizer will be in touch with you shortly",
-          template_id: "d-2497f4e79b5941ca9bd6a626cf94441d",
-        };
-        break;
-      default:
-        console.log("something wron");
-    }
-
-    console.log(`Received a submission: ${data.email}`);
-    await sgMail
-      .send(msg)
-      .then(() => {
-        console.log("Email sent");
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  } catch (error) {
-    return { statusCode: 422, body: String(error) };
+  console.log(data["form-name"]);
+  let msg = {};
+  switch (data["form-name"]) {
+    case "contact":
+      msg = {
+        to: data.email,
+        from: process.env.SENDER_EMAIL, // Change to your verified sender
+        subject: "Thank you for contakt us!",
+        text: "Thank you for contakt us!",
+        template_id: "d-f8c24a04d6314ea9a6e035933de4d34f",
+        dynamicTemplateData: {
+          name: data.contact_person,
+        },
+      };
+      break;
+    case "stand_application_form":
+      msg = {
+        to: data.email,
+        from: process.env.SENDER_EMAIL, // Change to your verified sender
+        subject: "Thank you for submitting your application",
+        text: "The organizer will be in touch with you shortly",
+        template_id: "d-2497f4e79b5941ca9bd6a626cf94441d",
+        dynamicTemplateData: {
+          name: data.contact_person,
+        },
+      };
+      break;
+    default:
+      console.log("something wron");
   }
+
+  console.log(`Received a submission: ${data.email}`);
+  await sgMail.send(msg);
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Email sent" }),

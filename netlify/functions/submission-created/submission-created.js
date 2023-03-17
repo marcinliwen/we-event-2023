b360dcbb01e57903b01db27b0b9c995dec52e8b7
m.liwen <m.liwen@perfectone.pl>
2023-03-17 13:33:49 +0100

    functiona

--- netlify/functions/submission-created/submission-created.js
+++ netlify/functions/submission-created/submission-created.js
@@ -1,37 +1,27 @@
-/* const process = require("process");
+const process = require("process");
 const sgMail = require("@sendgrid/mail");
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const handler = async (event) => {
-  console.log("event: ", event);
-  try {
-    const { data } = JSON.parse(event.body).payload;
+exports.handler = async (event) => {
+  const { data } = JSON.parse(event.body).payload;
 
-    const msg = {
-      to: "test@example.com", // Change to your recipient
-      from: process.env.SENDER_EMAIL, // Change to your verified sender
-      subject: "Sending with SendGrid is Fun",
-      text: "and easy to do anywhere, even with Node.js",
-      html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-    };
-    console.log(`Received a submission: ${data.email}`);
-    await sgMail
-      .send(msg)
-      .then(() => {
-        console.log("Email sent");
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  } catch (error) {
-    return { statusCode: 422, body: String(error) };
-  }
+  console.log(data["form-name"]);
+
+  const msg = {
+    to: data.email,
+    from: process.env.SENDER_EMAIL, // Change to your verified sender
+    subject: "Thank you for submitting your application",
+    text: "The organizer will be in touch with you shortly",
+    template_id: "d-2497f4e79b5941ca9bd6a626cf94441d",
+    dynamicTemplateData: {
+      name: data.contact_person,
+    },
+  };
+  console.log(`Received a submission: ${data.email}`);
+  await sgMail.send(msg);
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Email sent" }),
   };
 };
-
-module.exports = { handler };
- */
